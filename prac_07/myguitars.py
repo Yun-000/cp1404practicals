a01aebd816a9dfe03dac1dd9088d70302d507efb
luol. <yun.tao@my.jcu.edu.au>
2024-03-20 10:14:13 +0800

    Complete first part of More Guitars exercise

---
+++ prac_07/myguitars.py
@@ -0,0 +1,18 @@
+from prac_07.guitar import Guitar
+
+
+def main():
+    guitars = []
+    in_file = open('guitars.csv', 'r')
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+        guitars.append(guitar)
+    in_file.close()
+
+    for guitar in sorted(guitars):
+        print(guitar)
+
+
+main()
