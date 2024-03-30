7bae41c737fd4490a67d885a8a64a357dcbb0d90
luol. <yun.tao@my.jcu.edu.au>
2024-03-30 13:26:38 +0800

    Miles to Kilometres Converter

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,21 +1,34 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 
-class SquareNumberApp(App):
+class ConvertMileToKilometre(App):
+    output_text = StringProperty()
+
     def build(self):
-        Window.size = (300, 100)
-        self.title = "Square Number"
-        self.root = Builder.load_file('squaring.kv')
+        Window.size = (400, 200)
+        self.title = "Convert Mile to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_calculate(self, value):
+    def handle_convert(self, miles):
         try:
-            result = float(value) ** 2
-            self.root.ids.output_label.text = str(result)
+            kilometres = float(miles) * 1.60934
+            self.root.ids.output_label.text = str(kilometres)
         except ValueError:
-            pass
+            self.root.ids.output_label.text = '0.0'
+
+    def handle_increment(self, value):
+        try:
+            miles = float(self.root.ids.miles.text)
+            miles += value
+            self.root.ids.miles.text = str(miles)
+        except ValueError:
+            miles = 0
+            miles += value
+            self.root.ids.miles.text = str(miles)
 
 
-SquareNumberApp().run()
+ConvertMileToKilometre().run()
